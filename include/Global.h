40f14cfb23c5d1fe2f02238cd3f442f559ef8eac
Edward Soo <paktiksoo@gmail.com>
2013-03-30 00:00:49 -0700

    sky dome texture

--- include/Global.h
+++ include/Global.h
@@ -1,23 +1,8 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
 #pragma once
-#include <GL/gl.h>
-#include <GL/glut.h>
 
-// Day of year
-extern float day[9];
-
-extern float progress[9];
-
-extern float DAYS[9];
-extern bool isTimeProgressing;
-
-extern float getPlanetLocation(int planet);
-
-extern float speedFactor[10];
-
-extern void updateDay(int planet) ;
-
-extern void toggleTime();
-
-extern GLuint raw_texture_load(const char *filename, int width, int height);
-
-#define SKY 0
+GLuint raw_texture_load(GLuint textures[], const char *filename, int width,
+		int height, int texture_id);
+#endif
---
+++ include/main.h
@@ -0,0 +1,38 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define TITLE 0
+#define SKY_Q1 TITLE+1
+#define SKY_Q2 SKY_Q1+1
+#define SKY_Q3 SKY_Q2+1
+#define SKY_Q4 SKY_Q3+1
+#define NUM_TEXTURES SKY_Q4+1
+
+// flag to indicate that we should clean up and exit
+bool is_quit = false;
+
+// Keys variables
+bool key_states[256];
+bool special_states[256];
+
+// window handles
+int main_window, cam_window;
+unsigned long prev_time;
+int frame_passed = 0;
+char win_title[32] = { 0 };
+
+// Textures variables
+GLuint textures[NUM_TEXTURES];
+
+// display width and height
+int disp_width = 800, disp_height = 640;
+
+// Cursor previous position
+int prev_x, prev_y;
+
+//Cool debug tool! Use command line arguments to pass in ints!
+bool is_use_in_values = false;
+double in_value[3];
+double cam_rot_speed = 1.0;
+
+#endif
